a5b00722421a808c45b8c9767fd00a40e0447301
Maxim Dobryakov <max.dobryakov@ecmasoft.com>
2010-08-13 21:29:36 +0400

    Improved of query for select manager and subordinates

--- app/models/ecmasoft_notification_mailer.rb
+++ app/models/ecmasoft_notification_mailer.rb
@@ -2,8 +2,11 @@ require 'ostruct'
 
 class EcmasoftNotificationMailer < Mailer
 
+  def self.ecmasoft_group
+    @@ecmasoft_group ||= Group.find_by_lastname EcmasoftConsts::ECMASOFT_GROUP
+  end
+
   def self.calculate_developers_worktime
-    ecmasoft_group = Group.find_by_lastname EcmasoftConsts::ECMASOFT_GROUP
     users = ecmasoft_group.present? ? ecmasoft_group.users : []
 
     worktimes = []
@@ -17,7 +20,9 @@ class EcmasoftNotificationMailer < Mailer
               :week_worktime => week_worktime,
               :expected_week_worktime => expected_week_worktime,
               :month_worktime => month_worktime,
-              :expected_month_worktime => expected_month_worktime )
+              :expected_month_worktime => expected_month_worktime,
+              :show_warning =>  week_worktime / expected_week_worktime < 0.7 || month_worktime / expected_month_worktime < 0.7
+      )
     end
     worktimes
   end
@@ -37,53 +42,58 @@ class EcmasoftNotificationMailer < Mailer
   end
 
   def self.calculate_user_worktime(user, from, to)
-    current_hrs = TimeEntry.sum("hours", :conditions => ["user_id = ? AND spent_on BETWEEN ? AND ?", user.id, from, to]).round
+    current_hrs = TimeEntry.sum("hours", :conditions => ["user_id = ? AND spent_on BETWEEN ? AND ?", user.id, from, to])
     expected_hrs = 8 * CalendarStatusItem.workdays_count(from, to, user.id)
 
     [current_hrs, expected_hrs]
   end
 
-  def self.get_managers_and_related_stuff
-    ecmasoft_group = Group.find_by_lastname EcmasoftConsts::ECMASOFT_GROUP
-    ecmasoft_user_ids = ecmasoft_group.present? ? ecmasoft_group.users.map(&:id) : []
-
-    managers = {}
+  def self.get_managers
     manager_role = Role.find_by_name("Manager")
-    manager_members = manager_role.members.all(:include => [:user, :project]).select {|m| m.user.active? }
-    manager_members.each {|member| managers[member.user] = [] }
+    user_ids = manager_role.members.map(&:user_id).uniq
 
-    manager_members.each do |member|
-      manager = member.user
+    User.all(:conditions => ["users.id IN (?) AND users.status = 1 AND groups_users_join.group_id = ?", user_ids, ecmasoft_group.id],
+             :joins => [:groups])
+  end
 
-      member.project.members.each do |project_member|
-        user = project_member.user
+  def self.get_subordinates(manager)
+    manager_projects = Project.all(:joins => :users, :conditions => ["users.id = ?", manager.id])
+    projects_users_ids = Member.all(:select => "DISTINCT user_id", :conditions => ["project_id in (?) AND user_id != ?", manager_projects.map(&:id), manager.id])
+    User.all(:conditions => ["users.id IN (?) AND users.status = 1 AND groups_users_join.group_id = ?", projects_users_ids.map(&:user_id), ecmasoft_group.id], :joins => [:groups])
+  end
 
-        if ecmasoft_user_ids.include?(user.id) && user != manager && user.active?
-          managers[manager] << user unless managers[manager].include?(user)
-        end
-      end
+  def self.get_managers_and_subordinates
+    result = {}
+
+    managers = get_managers
+    managers.each do |manager|
+      result[manager] = get_subordinates(manager) 
     end
 
-    managers
+    result
   end
 
   def self.prepare_and_send_notification
-    managers = get_managers_and_related_stuff
+    managers = get_managers_and_subordinates
     worktimes = calculate_developers_worktime
 
     managers.each do |manager, subordinates|
       subordinate_ids = subordinates.map(&:id)
 
       if [52].include?(manager.id)
-        deliver_notification(manager, worktimes.select {|w| subordinate_ids.include?(w.user_id) } )
+        selected_worktimes = worktimes.select {|w| subordinate_ids.include?(w.user_id) }.sort_by(&:week_worktime) 
+        deliver_notification(manager, selected_worktimes)
       end
     end
+    nil
   end
 
   def notification(manager, subordinates)
+    title = (subordinates.any?{|s| s.show_warning } ? "[NEED ATTENTION] " : "") + "Timesheet notification"
+
     from "blog4m@yandex.ru" #"no-reply@ecmasoft.com"
     recipients manager.mail
-    subject "Timesheet notification"
+    subject title
     body :subordinates => subordinates
     content_type "text/html"
   end
